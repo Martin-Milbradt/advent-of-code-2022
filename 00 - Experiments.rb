6138c88b281a0d74a9fd40865aafe50ef774b10e
Milli <onethenumbernottheword@gmail.com>
2022-12-05 16:22:47 +0100

    experiments array merge

--- 00 - Experiments.rb
+++ 00 - Experiments.rb
@@ -1,30 +1,40 @@
-input = "
-                [M]     [W] [M]
-            [L] [Q] [S] [C] [R]
-            [Q] [F] [F] [T] [N] [S]
-    [N]     [V] [V] [H] [L] [J] [D]
-    [D] [D] [W] [P] [G] [R] [D] [F]
-[T] [T] [M] [G] [G] [Q] [N] [W] [L]
-[Z] [H] [F] [J] [D] [Z] [S] [H] [Q]
-[B] [V] [B] [T] [W] [V] [Z] [Z] [M]
- 1   2   3   4   5   6   7   8   9
-"
+def array_merge
+  a = ["1", "2"]
+  b = ["3", "4"]
 
+  def details(a)
+    "a: #{a} (length #{a.length})\n\n"
+  end
 
-lines = input.split("\n")
-
-grid = lines.map do |line|
-  line.chars
+  puts "+= : #{a += b}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "append : #{a.append(b)}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "concat : #{a.concat(b)}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "push : #{a.push(b)}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "push *: #{a.push(*b)}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "unshift *: #{a.unshift(*b)}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "unshift: #{a.unshift(b)}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "<<: #{a << b}\n"
+  puts details(a)
+  a = ["1", "2"]
+  puts "+: #{a + b}\n"
+  puts details(a)
 end
 
-puts grid.length
-puts grid[1].length
 
-grid = grid.transpose
-
-grid = lines.map do |line|
-  line.chars
+if __FILE__ == $0
+  array_merge
 end
-
-puts grid.length
-puts grid[1].length
